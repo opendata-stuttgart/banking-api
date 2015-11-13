8bc0227af2c568822a5384bfd1522c99b6ff8bed
Andreas Madsack <andreas@madflex.de>
2015-11-13 20:59:26 +0100

    new app and urls added

---
+++ banking/main/urls.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+from rest_framework import routers
+from django.conf.urls import patterns, include, url
+
+router = routers.DefaultRouter()
+
+urlpatterns = patterns(
+    '',
+    url(
+        regex=r'^', view=include(router.urls)
+    ),
+)
