89d2722c012bb1163e7d55b24e911c4ee5f1c1d8
Andreas Madsack <andreas@madflex.de>
2015-11-18 00:58:02 +0100

    for now use ports and no sockets

--- banking/start.sh
+++ banking/start.sh
@@ -7,4 +7,4 @@ mkdir -p /home/uid1000/banking/run
 chmod -R 777 /home/uid1000/banking/run
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
-gunicorn banking.wsgi:application --log-level=info --bind=unix:/home/uid1000/banking/run/server.sock -w 3
+gunicorn banking.wsgi:application --log-level=info --http 0.0.0.0:8000 --master --processes 4 --threads 2
