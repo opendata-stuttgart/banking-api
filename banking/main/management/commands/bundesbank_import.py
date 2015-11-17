d03f2a03959fb4485e737dc0a4c46ade10515050
Andreas Madsack <andreas@madflex.de>
2015-11-17 21:02:45 +0100

    fix setting of bic for same bank

--- banking/main/management/commands/bundesbank_import.py
+++ banking/main/management/commands/bundesbank_import.py
@@ -9,8 +9,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         from main.models import Bank
         from main.utils import get_rows_from_blz_excel, extract_data
+        prev_bic = None
         for element in get_rows_from_blz_excel():
             d = extract_data(element)
             dataset_number = d.pop('dataset_number')
+            if not d['bic'] and prev_bic:
+                d['bic'] = prev_bic
             b, x = Bank.objects.get_or_create(dataset_number=dataset_number,
                                               defaults=d)
+            prev_bic = d['bic']
