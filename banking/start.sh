62c7f68892a95bbda26da9a30341dc56bee7832e
Andreas Madsack <andreas@madflex.de>
2015-11-19 12:59:12 +0100

    fixes

--- banking/start.sh
+++ banking/start.sh
@@ -4,8 +4,9 @@ export PYTHONUNBUFFERED=0
 mkdir -p /home/uid1000/banking
 mkdir -p /home/uid1000/banking/logs
 mkdir -p /home/uid1000/banking/run
+mkdir -p /home/uid1000/banking/static
 chmod -R 777 /home/uid1000/banking/run
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
-gunicorn feinstaub.wsgi:application --log-level=info --bind=unix:/home/uid1000/banking/run/server.sock -w 3
+gunicorn banking.wsgi:application --log-level=info --bind=unix:/home/uid1000/banking/run/server.sock -w 3
 # exec uwsgi --http 0.0.0.0:8010 --wsgi-file /opt/code/banking/banking/wsgi.py --master --processes 4 --threads 2
