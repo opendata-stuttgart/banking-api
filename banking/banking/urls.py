78cbd95a8df232657f83f218a964105b9384db73
Andreas Madsack <andreas@madflex.de>
2015-12-03 13:58:29 +0100

    readonly api doesn't need login

--- banking/banking/urls.py
+++ banking/banking/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic.base import RedirectView
-from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
@@ -9,7 +8,4 @@ urlpatterns = [
 
     url(r'^$', RedirectView.as_view(url='/v1/', permanent=False)),
     url(r'^v1/', include('main.urls')),
-    url(r'^auth/', include('rest_framework.urls',
-                           namespace='rest_framework')),
-    url(r'^get-auth-token/', obtain_auth_token),
 ]
