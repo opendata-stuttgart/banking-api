1966b38167dde0cc67cd4dbc889fb3cce4ca86ad
Andreas Madsack <andreas@madflex.de>
2016-03-05 13:19:46 +0100

    move nginx for use with more than one virtualhost

--- update.sh
+++ update.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-git pull
+#git pull
 docker build --tag=banking-prod .
 docker rm -f banking
 docker run -d --volumes-from banking-data --link banking-db:db -v `pwd`/banking/banking/settings/production.py:/opt/code/banking/banking/settings/production.py --restart=always --name banking banking-prod
-docker rm -f banking-nginx
-docker run --name banking-nginx --net="host" --volumes-from banking-data -p 80:80 -v `pwd`/nginx.conf:/etc/nginx/nginx.conf --restart=always -d nginx
+#docker rm -f banking-nginx
+#docker run --name banking-nginx --net="host" --volumes-from banking-data -p 80:80 -v `pwd`/nginx.conf:/etc/nginx/nginx.conf -v `pwd`/banking-nginx.conf:/etc/nginx/banking-nginx.conf --restart=always -d nginx
+
+bash nginx.sh
 
 echo "Cleaning up old docker images..."
 docker rmi $(docker images | grep "<none>" | awk '{print($3)}')
