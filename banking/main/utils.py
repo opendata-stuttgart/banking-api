6d61d96155e17d95f51a19705df95c578d3a99cb
Andreas Madsack <andreas@madflex.de>
2017-03-24 19:20:16 +0100

    new blz file from bundesbank

--- banking/main/utils.py
+++ banking/main/utils.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def get_rows_from_blz_excel(data_file=None):
     if not data_file:
-        data_file = "http://www.bundesbank.de/Redaktion/DE/Downloads/Aufgaben/Unbarer_Zahlungsverkehr/Bankleitzahlen/2016_03_06/blz_2015_12_07_xls.xlsx?__blob=publicationFile"
+        data_file = "https://www.bundesbank.de/Redaktion/DE/Downloads/Aufgaben/Unbarer_Zahlungsverkehr/Bankleitzahlen/2017_06_04/blz_2017_03_06_xls.xlsx?__blob=publicationFile"
     data = pd.ExcelFile(data_file).parse()
     # replace float with value `nan` with None
     data_with_none = data.where((pd.notnull(data)), None)
