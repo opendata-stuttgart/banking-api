39335649a7f0c6a6b8756af939cfc341e6689947
Andreas Madsack <andreas@madflex.de>
2017-06-15 22:59:27 +0200

    change for current production env

--- update.sh
+++ update.sh
@@ -5,9 +5,7 @@ set -e
 #git pull
 docker build --tag=banking-prod .
 docker rm -f banking
-docker run -d --volumes-from banking-data --link banking-db:db -v `pwd`/banking/banking/settings/production.py:/opt/code/banking/banking/settings/production.py --restart=always --name banking banking-prod
-#docker rm -f banking-nginx
-#docker run --name banking-nginx --net="host" --volumes-from banking-data -p 80:80 -v `pwd`/nginx.conf:/etc/nginx/nginx.conf -v `pwd`/banking-nginx.conf:/etc/nginx/banking-nginx.conf --restart=always -d nginx
+docker run -d --volumes-from home-data --link banking-db:db -v `pwd`/banking/banking/settings/production.py:/opt/code/banking/banking/settings/production.py --restart=always --name banking banking-prod
 
 bash nginx.sh
 
